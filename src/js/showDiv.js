7650b471c2e7c59c4ee3b133564f37e5f2468d8f
B-Rios <alrios4319@gmail.com>
2022-06-22 02:08:20 +0000

    perf: cleared code

--- src/js/showDiv.js
+++ src/js/showDiv.js
@@ -10,13 +10,13 @@ divQuantity.forEach((btn) => app(btn));
 function app(btn) {
   btn.addEventListener("click", (e) => {
     const indexNumber = e.target.dataset.indexNumber;
-    clearDivs(indexNumber);
+    clearDivs();
     showDivs(indexNumber);
     // console.log(indexNumber);
   });
 }
 
-const clearDivs = (data) => {
+const clearDivs = () => {
   for (let i = 0; i < divs.length; i++) {
     divs[i].style.display = "none";
   }
