487856856d0df603cce05186f271653bbb2c915a
B-Rios <alrios4319@gmail.com>
2022-06-22 01:03:27 +0000

    fix: receiving data-index-number

--- src/js/showDiv.js
+++ src/js/showDiv.js
@@ -1,28 +1,32 @@
-const divs = document.querySelectorAll('.child')
-const divQuantity = document.querySelectorAll('.div-num')
-const selectedDivs = btn.dataset.indexNumber
+const divs = document.querySelectorAll(".child");
+const divQuantity = document.querySelectorAll(".div-num");
 
 const randomic = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-divQuantity.forEach(btn => app(btn));
+divQuantity.forEach((btn) => app(btn));
 
 function app(btn) {
-    btn.addEventListener('click', () => {clearDivs, showDivs});
+  btn.addEventListener("click", (e) => {
+    const indexNumber = e.target.dataset.indexNumber;
+    clearDivs(indexNumber);
+    showDivs(indexNumber);
+    // console.log(indexNumber);
+  });
 }
 
-const clearDivs = () => {
-    for (let i = 0; i < divs.length; i++){
-            divs[i].style.display = 'none';
-        }
-}
+const clearDivs = (data) => {
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].style.display = "none";
+  }
+};
 
-const showDivs = () => {
-    for (let i = 0; i < selectedDivs; i++) {
-            const randomH = randomic(100, 300)
-            const element = divs[i]
-            element.style.display = 'block';
-            element.style.height = `${randomH}px`        
-        }
-}
+const showDivs = (data) => {
+  for (let i = 0; i < data; i++) {
+    const randomH = randomic(100, 300);
+    const element = divs[i];
+    element.style.display = "block";
+    element.style.height = `${randomH}px`;
+  }
+};
